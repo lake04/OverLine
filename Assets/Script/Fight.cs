5dd826a22b090ab7aa13ac388d1bed3f528a6f9d
lake04 <wjdghksrla0409@gmail.com>
2025-02-18 00:41:44 +0900

    .

---
+++ Assets/Script/Fight.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fight : MonoBehaviour
+{
+    int a = 50;
+    int b;
+
+
+    void Start()
+    {
+        
+            //0~100
+            int result = Random.Range(0, 100);
+            Debug.Log($"{result} ,{a}");
+
+            if (result >= a)
+            {
+                Debug.Log("합 승리");
+                a--;
+
+            }
+            else
+            {
+                Debug.Log("합 패배");
+
+                a++;
+            }
+    }
+}
