e65e598b094f52f9e1098314846f5c4b53bb6c30
moyung16 <sdh240201@sdh.hs.kr>
2025-02-20 02:23:22 +0900

    ui

---
+++ Assets/Script/Stage/GoogleSeet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GoogleSeet : MonoBehaviour
+{
+    string sheetData;
+    const string googleSheetURL = " https://docs.google.com/spreadsheets/d/1OgoqytMNyU_cVTYXuy4bOffSAB76rp4h9dZ6FZSyviE/export?format=tsv&range=A5:I7";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
