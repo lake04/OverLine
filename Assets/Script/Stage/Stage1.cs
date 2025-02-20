3cd4b26149b1bd6b241e3542ea785ed8442aa1e9
moyung16 <sdh240201@sdh.hs.kr>
2025-02-20 17:56:18 +0900

    스테이지

---
+++ Assets/Script/Stage/Stage1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1 : MonoBehaviour
+{
+    public void stage1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
